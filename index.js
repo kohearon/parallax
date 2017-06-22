2d890fa88b03b6c31b92015e4c5a6e772f4acb1a
Kevin-Geoffery O'Hearon <kohearon@modernmedia.ca>
2017-06-22 16:26:39 -0300

    initial commit.

---
+++ index.js
@@ -0,0 +1,27 @@
+document.addEventListener('scroll', parallaxText);
+
+function parallaxText(){
+
+  /* Get Position of Top of Image, Top of User's Window */
+  const topOfImage = document.querySelector('.parallax__main-image').offsetTop;
+  const topOfPage = window.pageYOffset;
+
+  document.querySelector('.parallax__text-container').style.transform = `translateY(${ -0.1 * topOfPage - 400 }px)`
+}
+
+document.addEventListener('scroll', parallaxImages);
+
+function parallaxImages(){
+
+  /* Get Position of Top of Image, Top of User's Window */
+  const topOfImage = document.querySelector('.second-parallax__main-image').offsetTop;
+  const topOfPage = window.pageYOffset;
+
+  /* If The User's Window Is At The Top of The Image,
+  ** Start Parallax Until Image Reach The Top of The User's Screen
+  */
+  if (topOfPage > ( topOfImage - 700 ) ) {
+
+    document.querySelector('.second-parallax__secondary-image').style.transform = `translateY(${ -0.1 * topOfPage - 130 }px)`
+  }
+}

dab3efa5d2894d0d49a2bd71dff40d917fa929d6
Kevin-Geoffery O'Hearon <kohearon@modernmedia.ca>
2017-06-23 12:18:07 -0300

    happy with small parallaxText function.

--- index.js
+++ index.js
@@ -25,3 +25,18 @@ function parallaxImages(){
     document.querySelector('.second-parallax__secondary-image').style.transform = `translateY(${ -0.1 * topOfPage }px)`
   }
 }
+
+document.addEventListener('scroll', parallaxDivider);
+
+function parallaxDivider(){
+
+  /* Get Position of Top of Image, Top of User's Window */
+  const topOfImage = document.querySelector('.second-parallax__main-image').offsetTop;
+  const topOfPage = window.pageYOffset;
+
+  /* If The User's Window Is At The Top of The Image,
+  ** Start Parallax Until Image Reach The Top of The User's Screen
+  */
+ document.querySelector('.section-divider').style.transform = `translateY(${ -0.05 * topOfPage }px)`
+
+}
